922588c3c1cba31df018fd1439b82dbc64472b0e
admin01 <paradoxraphael@gmai.com>
2024-07-15 16:29:59 +1000

    connecting supabase with the app

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://kyjkvmtqkfvbenttjrma.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt5amt2bXRxa2Z2YmVudHRqcm1hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEwMTkyNTksImV4cCI6MjAzNjU5NTI1OX0.V7wE07QllKOgophn69YVdZj1QW-W13FZsgV8DEt-k3A";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
