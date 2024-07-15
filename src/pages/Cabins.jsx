734e3d6740a330f3aedbc5f88ac7109adcfc1ee5
admin01 <paradoxraphael@gmai.com>
2024-07-15 16:36:55 +1000

    setting up storage bucket

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -10,7 +10,7 @@ function Cabins() {
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
+      <img src="https://kyjkvmtqkfvbenttjrma.supabase.co/storage/v1/object/public/cabin-images/cabin_001.jpg" />
     </Row>
   );
 }
